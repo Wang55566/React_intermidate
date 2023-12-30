7d9398833c47c36a0db3797494484865ed692322
Wang55566 <wang6@hawaii.edu>
2023-12-30 14:50:11 -0800

    m

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,31 +1,17 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import usePosts from './hooks/postTodos';
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  const { data, error, isLoading } = usePosts()
 
-  if (error) return <p>{error}</p>;
+
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
+      {data?.map((data) => (
+        <li key={data.id} className="list-group-item">
+          {data.title}
         </li>
       ))}
     </ul>
