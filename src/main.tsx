3a4ac9d11acda3f903f710fdeffa2c3685b59211
Wang55566 <wang6@hawaii.edu>
2023-12-30 13:58:36 -0800

    change stale time

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient({
-  
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
