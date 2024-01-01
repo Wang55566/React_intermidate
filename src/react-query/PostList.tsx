2d76824edc4d1d9343e652ccbbdca056394ab7b5
Wang55566 <wang6@hawaii.edu>
2024-01-01 13:15:07 -0800

    pagenation

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,24 +2,17 @@ import { useState } from 'react';
 import usePosts from './hooks/postTodos';
 
 const PostList = () => {
-
-  const [ userId, setUserId ]= useState<number>()
-  const { data, error, isLoading } = usePosts(userId)
+  const pageSize = 10;
+  const [ page, setPage ] = useState(1);
+  const { data, error, isLoading } = usePosts({
+    page, pageSize
+  });
 
 
   if (error) return <p>{error.message}</p>;
 
   return (
     <>
-      <select
-        onChange={(e) => setUserId(parseInt(e.target.value))}
-        value={userId}
-        className="form-select mb-3">
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select>
       <ul className="list-group">
         {data?.map((data) => (
           <li key={data.id} className="list-group-item">
@@ -27,6 +20,15 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page===1}
+        className="btn btn-primary my-3"
+        onClick={() => setPage(page - 1)}
+        >Previous</button>
+      <button
+        className="btn btn-primary my-3 ms-1"
+        onClick={() => setPage(page + 1)}
+        >Next</button>
     </>
   );
 };
