bcc44aba95e32bc38bc597b945ef326b9ef35ecd
Wang55566 <wang6@hawaii.edu>
2024-01-01 13:00:59 -0800

    commit

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,20 +1,33 @@
+import { useState } from 'react';
 import usePosts from './hooks/postTodos';
 
 const PostList = () => {
 
-  const { data, error, isLoading } = usePosts()
+  const [ userId, setUserId ]= useState<number>()
+  const { data, error, isLoading } = usePosts(userId)
 
 
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {data?.map((data) => (
-        <li key={data.id} className="list-group-item">
-          {data.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        onChange={(e) => setUserId(parseInt(e.target.value))}
+        value={userId}
+        className="form-select mb-3">
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {data?.map((data) => (
+          <li key={data.id} className="list-group-item">
+            {data.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
