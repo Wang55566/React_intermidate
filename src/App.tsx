3c38c665faa9610fd80e5faebc72fedc1ed13bb3
Wang55566 <wang6@hawaii.edu>
2024-01-02 12:28:29 -0800

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import TodoList from './react-query/TodoList';
-import TodoForm from './react-query/TodoForm';
+import TaskList from './state-management/TaskList';
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <TaskList />
     </>
   )
 }
