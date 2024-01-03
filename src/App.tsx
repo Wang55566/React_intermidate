9b4a30f6ad9a07e7630b8b59652fd6583ab9b1af
Wang55566 <wang6@hawaii.edu>
2024-01-03 10:33:32 -0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { useReducer } from 'react';
 import './App.css';
-import tasksReducer from './state-management/reducers/tasksReducer';
-import TasksContext from './state-management/contexts/tasksContext';
-import NavBar from './state-management/NavBar';
+import AuthProvider from './state-management/AuthProvider';
 import HomePage from './state-management/HomePage';
-import authReducer from './state-management/reducers/authReducer';
-import AuthContext from './state-management/contexts/authContext';
+import NavBar from './state-management/NavBar';
+import TasksProvider from './state-management/TasksProvider';
 
 function App() {
 
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, [])
-  const [user, authDispatch] = useReducer(authReducer, '')
-
   return (
-    <AuthContext.Provider value={{user, dispatch: authDispatch}}>
-      <TasksContext.Provider value={{tasks, dispatch: tasksDispatch}}>
+    <AuthProvider>
+      <TasksProvider>
         <NavBar />
         <HomePage />
-      </TasksContext.Provider>
-    </AuthContext.Provider>
+      </TasksProvider>
+    </AuthProvider>
   )
 }
 
