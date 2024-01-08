51e984de6282bfe2137d66819556f365bc59b89d
Wang55566 <wang6@hawaii.edu>
2024-01-07 22:35:20 -0800

    private route

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,19 +3,31 @@ import HomePage from "./HomePage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
       { path: "users",
         element: <UsersPage />,
         children: [
           { path: ":id", element: <UserDetail /> },
-        ]},
-    ],
+        ]}
+
+    ]
+
   },
 ]);
 
